2803d6661b908175dff042d8f5933194776d3951
navesh123 <navesh2005@yahoo.com>
2019-08-01 12:23:16 +0200

    1

--- wfe.sh
+++ wfe.sh
@@ -4,8 +4,8 @@ sudo apt-get install openjdk-8-jre -y
 sudo apt-get install git -y
 sudo apt-get install make -y
 sudo apt-get install make-guile -y
-git clone https://github.com/ThoughtWorksInc/infra-problem
-sudo curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o /usr/local/bin/lein
+git clone "https://github.com/ThoughtWorksInc/infra-problem"
+sudo curl "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein" -o /usr/local/bin/lein
 sudo chmod a+x /usr/local/bin/lein
 cd /home/testadmin/infra-problem
 make libs
