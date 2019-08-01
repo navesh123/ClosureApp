f4bdd684f31bf1bfdd7d15ef383c3e1d5a96ecd4
navesh123 <navesh2005@yahoo.com>
2019-08-01 12:41:05 +0200

    1

--- wfe.sh
+++ wfe.sh
@@ -8,15 +8,16 @@ sudo apt-get install make-guile -y
 git clone "https://github.com/ThoughtWorksInc/infra-problem"
 sudo curl "https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein" -o /usr/local/bin/lein
 sudo chmod a+x /usr/local/bin/lein
-cd /home/testadmin/infra-problem
+cd infra-problem
 make libs
 make clean all
-cd /home/testadmin/infra-problem/front-end/public
+cd front-end/public
 py serve.py
 export APP_PORT="8080"
 export STATIC_URL="http://closure-wfe"
 export QUOTE_SERVICE_URL="http://closure-quotes"
 export NEWSFEED_SERVICE_URL="http://closure-newsfeed"
 export NEWSFEED_SERVICE_TOKEN="T1&eWbYXNWG1w1^YGKDPxAWJ@^et^&kX"
-cd /home/testadmin/infra-problem/build
+cd ..
+cd build
 java -jar front-end.jar
